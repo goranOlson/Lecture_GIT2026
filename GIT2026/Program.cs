d675d68f33dbdd00673d1c8549c7b759cead9d82
Göran Olson <goran@famolson.se>
2026-05-20 12:00:31 +0200

    Some more code...

--- GIT2026/Program.cs
+++ GIT2026/Program.cs
@@ -4,13 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            int a = 1;
+            int b = 2;
 
+            Sum(a, b);
 
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            foreach (int number in numbers)
+            {
+                int x = number;
+                Console.WriteLine(x);
+            }
         }
+           
+        private static void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
